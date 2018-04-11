4d9f0a46efb40962581de8aceb10d6856f2b280c
I318734 <shahar.harari@sap.com>
2018-04-11 05:37:10 +0000

    test

---
+++ src/main/java/com/sap/lsp/cf/ws/IdleTime2.java
@@ -0,0 +1,18 @@
+package com.sap.lsp.cf.ws;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(description = "Idle Time", urlPatterns = {"/IdleTime/*"})
+public class IdleTime2 extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write(Long.toString(IdleTimeHolder.getInstance().getIdleTime()));
+        }
+    }
+}
